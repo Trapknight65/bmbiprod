707320d2ae900228292882a6d70a1be8b8e9c3ed
Trapknight65 <trapknight65@gmail.com>
2025-12-07 12:56:25 +0000

    Fix Header import path in MultiStepForm.js

--- src/components/forms/MultiStepForm.js
+++ src/components/forms/MultiStepForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Header from './Header';
+import Header from '../Header';
 
 export default function MultiStepForm() {
     const [step, setStep] = useState(1);
